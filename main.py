6ed99c9189c18f7b8517c917defc6a90bf3e0e3d
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-09-17 22:11:47 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print('#######################')
+print('# Ask To Oraculo Game #')
+print('#######################')
+
+sequencia_numerica = 43
+
+tentativa = input('Adivinhe a sequência numérica, digite um número:')
+print('Você digitou:', tentativa)
+
+if (tentativa == sequencia_numerica):
+    print('Adivinhou, parabéns!')
+else:
+    print('Infelizmente, vocẽ não acertou.')
+
