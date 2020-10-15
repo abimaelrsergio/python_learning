8dc02b13e8ea3bd9d988c298069dc84f88365e09
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-10-15 19:44:12 -0300

    Atualização

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ print('# Ask To Oráculo Game #')
 print('#######################')
 
 sequencia_numerica = 43
+numero_jogadas = 3
+round = 1
 
-tentativa = input('Adivinhe a sequência numérica, digite um número:')
-print('Você digitou:', tentativa)
+while(round <= numero_jogadas):
+    print('jogada {} de {}'.format(round, numero_jogadas))
+    tentativa = int(input('Adivinhe a sequência numérica, digite um número:'))
+    print('Você digitou:', tentativa)
 
-if (tentativa == sequencia_numerica):
-    print('Adivinhou, parabéns!')
-else:
-    print('Infelizmente, vocẽ não acertou.')
+    adivinhou = tentativa == sequencia_numerica
+    numero_maior = tentativa > sequencia_numerica
+    numero_menor = tentativa < sequencia_numerica
 
+    if adivinhou:
+        print('Adivinhou, parabéns!')
+    else:
+        if numero_maior:
+            print('Sua sugestão foi maior do que a sequência numérica')
+        elif numero_menor:
+            print('Sua sugetão foi menor do que a sequência numérica')
+        print('Infelizmente, vocẽ não acertou.')
+    print('----------------')
+    round = round + 1
+print('Game Over!')
