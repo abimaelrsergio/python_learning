19133e542c1fe5bfc98843ab8bda704abbe00a77
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-10-29 19:43:01 -0300

    Atualização

--- play.py
+++ play.py
@@ -3,3 +3,9 @@ print('# Selecione um jogo #')
 print('#####################')
 
 print('(1) Forca  (2) Oráculo')
+game = int(input('> '))
+
+if game == 1:
+    print('Forca')
+elif game == 2:
+    print('Oráculo')
