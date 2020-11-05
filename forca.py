073b2c764383251b58ff71131e2686ac56fcb449
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-11-05 19:37:57 -0300

    Atualização

--- forca.py
+++ forca.py
@@ -7,16 +7,21 @@ def play_forca():
     palavra_para_adivinhar = 'impressora'
     acertos = ['_','_','_','_','_','_','_','_','_','_']
     enforcado = False
+    total_tentativas = 0
     ganhou = False
     print(acertos)
     while not enforcado and not ganhou:
         tentativa = input('Informe uma letra: ')
         tentativa = tentativa.strip()
-        indice = 0
-        for letra in palavra_para_adivinhar:
-            if tentativa.upper() == letra.upper():
-                acertos[indice] = tentativa
-            indice = indice + 1
+        if tentativa in palavra_para_adivinhar:
+            indice = 0
+            for letra in palavra_para_adivinhar:
+                if tentativa.upper() == letra.upper():
+                    acertos[indice] = tentativa
+                indice = indice + 1
+        else:
+            total_tentativas = total_tentativas + 1
+        enforcado = total_tentativas == 7
         print(acertos)
 
     print('Fim do jogo')
